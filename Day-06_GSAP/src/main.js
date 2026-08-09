f1c2b1925f097861ca4fe53de2dbebecf2902e8b
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-08-09 22:43:10 +0530

    explore stagger

--- Day-06_GSAP/src/main.js
+++ Day-06_GSAP/src/main.js
@@ -1,36 +1,10 @@
 import "./style.css";
 import { gsap } from "gsap";
 
-const btns = document.querySelectorAll(".btn");
 
-btns.forEach((btn) => {
-  btn.addEventListener("mouseenter", () => {
-    if (btn.classList[1] === "btn-scale") {
-      gsap.to(btn, {
-        scale: 1.15,
-        duration: 0.3,
-      });
-    } else if (btn.classList[1] === "btn-bounce") {
-      gsap.to(btn, {
-        y: -10,
-        ease: "back.out(7)",
-      });
-    } else {
-      gsap.to(btn, {
-        backgroundColor: "green",
-        fontFamily: "apple",
-        fontSize: "50px",
-        transformOrigin: "bottom",
-      });
-    }
-  });
-  btn.addEventListener("mouseleave", () => {
-    gsap.to(btn, {
-      scale: 1,
-    });
-    gsap.to(btn, {
-      y: 0,
-      ease: "bounce.out",
-    });
-  });
-});
+gsap.to(".box" , {
+  x: 500,
+  duration: 3,
+  delay: 0.5,
+  stagger: -0.2
+})
