3487b671ecb501bb4e2007c558436e820198bfb2
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-08-09 22:19:03 +0530

    button Effects

--- Day-06_GSAP/src/main.js
+++ Day-06_GSAP/src/main.js
@@ -1,8 +1,36 @@
-import './style.css'
-import { gsap } from 'gsap'
+import "./style.css";
+import { gsap } from "gsap";
 
-gsap.to(".box" , {
-  x: 300,
-  duration: 2,
-  delay: 2,
-})
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((btn) => {
+  btn.addEventListener("mouseenter", () => {
+    if (btn.classList[1] === "btn-scale") {
+      gsap.to(btn, {
+        scale: 1.15,
+        duration: 0.3,
+      });
+    } else if (btn.classList[1] === "btn-bounce") {
+      gsap.to(btn, {
+        y: -10,
+        ease: "back.out(7)",
+      });
+    } else {
+      gsap.to(btn, {
+        backgroundColor: "green",
+        fontFamily: "apple",
+        fontSize: "50px",
+        transformOrigin: "bottom",
+      });
+    }
+  });
+  btn.addEventListener("mouseleave", () => {
+    gsap.to(btn, {
+      scale: 1,
+    });
+    gsap.to(btn, {
+      y: 0,
+      ease: "bounce.out",
+    });
+  });
+});
