9ddbecea3739a24ffd2690f3a446004313f8f6cb
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-08-10 11:32:21 +0530

    position parameter before end prev animation

--- Day-06_GSAP/src/main.js
+++ Day-06_GSAP/src/main.js
@@ -6,17 +6,24 @@ const tl = gsap.timeline();
 tl.to(".box1", {
   x: 500,
   duration: 1,
-}).to(".box2", {
-  x: 500,
-  duration: 1,
-}).to(".box3", {
-  x: 500,
-  duration: 1,
-},"+=0.9")
-.to(".box4", {
-  x: 500,
-  duration: 1,
-}).to(".box5", {
-  x: 500,
-  duration: 1,
 })
+  .to(".box2", {
+    x: 500,
+    duration: 1,
+  })
+  .to(
+    ".box3",
+    {
+      x: 500,
+      duration: 1,
+    },
+    "-=0.9",
+  )
+  .to(".box4", {
+    x: 500,
+    duration: 1,
+  })
+  .to(".box5", {
+    x: 500,
+    duration: 1,
+  });
