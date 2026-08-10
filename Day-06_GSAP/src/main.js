1a6f27f571f9d219316ea411a24d201c593061f4
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-08-10 11:27:08 +0530

    position parameter

--- Day-06_GSAP/src/main.js
+++ Day-06_GSAP/src/main.js
@@ -1,10 +1,22 @@
 import "./style.css";
 import { gsap } from "gsap";
 
+const tl = gsap.timeline();
 
-gsap.to(".box" , {
+tl.to(".box1", {
   x: 500,
-  duration: 3,
-  delay: 0.5,
-  stagger: -0.2
+  duration: 1,
+}).to(".box2", {
+  x: 500,
+  duration: 1,
+}).to(".box3", {
+  x: 500,
+  duration: 1,
+},"+=0.9")
+.to(".box4", {
+  x: 500,
+  duration: 1,
+}).to(".box5", {
+  x: 500,
+  duration: 1,
 })
