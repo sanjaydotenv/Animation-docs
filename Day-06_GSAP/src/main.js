62c9c33587e63c248ef2e3b0ee7721dc710c9790
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-08-10 22:57:58 +0530

    use of scrub and scrollTrigger

--- Day-06_GSAP/src/main.js
+++ Day-06_GSAP/src/main.js
@@ -1,3 +1,13 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".box", {
+  x: 500,
+  scrollTrigger: {
+    trigger: ".box",
+    markers: true,
+    start: "top 50%",
+    scrub: 1,
+  },
+});
