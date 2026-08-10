bb3379b4105f5debac5391fcc57c4d397c25f0c0
Sanjay Bairagi <mayur92mayur@gmail.com>
2026-08-10 13:28:01 +0530

    label in gsap

--- Day-06_GSAP/src/main.js
+++ Day-06_GSAP/src/main.js
@@ -2,28 +2,17 @@ import "./style.css";
 import { gsap } from "gsap";
 
 const tl = gsap.timeline();
-
-tl.to(".box1", {
+tl.addLabel("mylab")
+tl.to(".box1" , {
   x: 500,
   duration: 1,
-})
-  .to(".box2", {
-    x: 500,
-    duration: 1,
-  })
-  .to(
-    ".box3",
-    {
-      x: 500,
-      duration: 1,
-    },
-    "-=0.9",
-  )
-  .to(".box4", {
-    x: 500,
-    duration: 1,
-  })
-  .to(".box5", {
-    x: 500,
-    duration: 1,
-  });
+}, "mylab").to(".box2" , {
+  x: 500,
+  duration: 1
+} , "-=0.9").to(".box3" , {
+  x: 500,
+  duration: 1
+}).to(".box4" , {
+  x: 500,
+  duration: 1
+},"mylab")
